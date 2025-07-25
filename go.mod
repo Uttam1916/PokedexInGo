d3387e622a71ee97f6d820eca8b846d44e454de1
Uttam <tired3120@gmail.com>
2025-07-25 17:32:55 +0530

    added repl capabilites and basic commands

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/Uttam1916/PokedexInGo
-
-go 1.24.5
--- main.go
+++ main.go
@@ -1,8 +1,72 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+// mamke commands globally accesible
+var commands = map[string]cliCommand{}
+
+func main() {
+
+	commands["exit"] = cliCommand{
+		name:        "exit",
+		description: "Exits the program",
+		callback:    commandExit,
+	}
+	commands["help"] = cliCommand{
+		name:        "help",
+		description: "Displays a help message",
+		callback:    commandHelp,
+	}
+
+	// create a scanner to read line by line
+	scanner := bufio.NewScanner(os.Stdin)
+	//infinite for loop to wait for user input
+	fmt.Println("Pokedex > Hello!")
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+
+		//check if command exists
+		cmd, ok := commands[input]
+		if !ok {
+			fmt.Printf("Unkown command")
+			continue
+
+		}
+
+		err := cmd.callback()
+		if err != nil {
+			fmt.Println("Something went wrong : ", err)
+		}
+	}
+}
+
+func commandHelp() error {
+	fmt.Println("Usages:")
+	for _, com := range commands {
+		fmt.Printf("%v : %v\n", com.name, com.description)
+	}
+	return nil
+}
+
+func commandExit() error {
+	fmt.Println("Thank you for using Pokedex!")
+	os.Exit(0)
+	return nil
+}
 
 func cleanInput(text string) []string {
 	text = strings.TrimSpace(text)
