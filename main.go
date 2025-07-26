2faecbbcb317b6f1e1ef488f8bcad78321e64cde
Uttam <tired3120@gmail.com>
2025-07-26 14:39:10 +0530

    added map command

--- main.go
+++ main.go
@@ -2,10 +2,27 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"strings"
 )
+
+type config struct {
+	Next     string
+	Previous string
+}
+
+type jsonResult struct {
+	Result []locationAreas `json:"results"`
+}
+
+type locationAreas struct {
+	Name string `json:"name"`
+	Url  string `json:"url"`
+}
 
 type cliCommand struct {
 	name        string
@@ -27,6 +44,11 @@ func main() {
 		name:        "help",
 		description: "Displays a help message",
 		callback:    commandHelp,
+	}
+	commands["map"] = cliCommand{
+		name:        "map",
+		description: "Prints 20 locations",
+		callback:    commandMap,
 	}
 
 	// create a scanner to read line by line
@@ -65,6 +87,34 @@ func commandHelp() error {
 func commandExit() error {
 	fmt.Println("Thank you for using Pokedex!")
 	os.Exit(0)
+	return nil
+}
+
+func commandMap() error {
+	// make the request
+	res, err := http.Get("https://pokeapi.co/api/v2/location-area")
+	if err != nil {
+		return fmt.Errorf("error making request", err)
+	}
+	defer res.Body.Close()
+	// create a slice of bytes from the io reader
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return fmt.Errorf("error creating body", err)
+	}
+	if res.StatusCode > 299 {
+		return fmt.Errorf("Status code >299")
+	}
+	// decode the bytes into a struct
+	var areas jsonResult
+	err = json.Unmarshal(body, &areas)
+	if err != nil {
+		return fmt.Errorf("couldnt decode json to struct")
+	}
+	for _, area := range areas.Result {
+		fmt.Printf("%s\n", area.Name)
+	}
+
 	return nil
 }
 
